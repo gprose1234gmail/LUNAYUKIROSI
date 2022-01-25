916e4a6c98196a0d2b3aece4f5134ddb97d94087
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-01-25 21:25:19 +0530

    Update cli.py

--- Yukki/Core/Clients/cli.py
+++ Yukki/Core/Clients/cli.py
@@ -1,7 +1,6 @@
 from pyrogram import Client
 
-from config import (API_HASH, API_ID, BOT_TOKEN, STRING1, STRING2, STRING3,
-                    STRING4, STRING5)
+from config import (API_HASH, API_ID, BOT_TOKEN, STRING1)
 
 app = Client(
     "YukkiMusicBot",
@@ -11,7 +10,4 @@ app = Client(
 )
 
 ASS_CLI_1 = Client(STRING1, API_ID, API_HASH)
-ASS_CLI_2 = Client(STRING2, API_ID, API_HASH)
-ASS_CLI_3 = Client(STRING3, API_ID, API_HASH)
-ASS_CLI_4 = Client(STRING4, API_ID, API_HASH)
-ASS_CLI_5 = Client(STRING5, API_ID, API_HASH)
+
