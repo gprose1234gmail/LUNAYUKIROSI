20b9a92a60ff57d612e238bfbc400edeef2e0d9e
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-01-25 21:35:27 +0530

    Update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -7,7 +7,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineQueryResultPhoto, InputTextMessageContent,
                             Message)
 
-from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
+from Yukki import (ASS_CLI_1,
                    ASSISTANT_PREFIX, BOT_ID, BOT_USERNAME, LOG_GROUP_ID,
                    MUSIC_BOT_NAME, SUDOERS, app)
 from Yukki.Database import (approve_pmpermit, disapprove_pmpermit,
@@ -54,46 +54,7 @@ flood = {}
     & ~filters.via_bot
     & ~filters.user(SUDOERS)
 )
-@ASS_CLI_2.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
-@ASS_CLI_3.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
-@ASS_CLI_4.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
-@ASS_CLI_5.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
+
 async def awaiting_message(client, message):
     user_id = message.from_user.id
     if await is_pmpermit_approved(user_id):
@@ -159,26 +120,6 @@ async def pm_approve(client, message):
     & filters.user(SUDOERS)
     & ~filters.via_bot
 )
-@ASS_CLI_2.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_3.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_4.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_5.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
 async def pm_disapprove(client, message):
     if not message.reply_to_message:
         return await eor(
@@ -203,26 +144,6 @@ async def pm_disapprove(client, message):
     & filters.user(SUDOERS)
     & ~filters.via_bot
 )
-@ASS_CLI_2.on_message(
-    filters.command("block", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_3.on_message(
-    filters.command("block", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_4.on_message(
-    filters.command("block", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_5.on_message(
-    filters.command("block", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
 async def block_user_func(client, message):
     if not message.reply_to_message:
         return await eor(message, text="Reply to a user's message to block.")
@@ -236,26 +157,7 @@ async def block_user_func(client, message):
     & filters.user(SUDOERS)
     & ~filters.via_bot
 )
-@ASS_CLI_2.on_message(
-    filters.command("unblock", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_3.on_message(
-    filters.command("unblock", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_4.on_message(
-    filters.command("unblock", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_5.on_message(
-    filters.command("unblock", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
+
 async def unblock_user_func(client, message):
     if not message.reply_to_message:
         return await eor(
@@ -271,26 +173,7 @@ async def unblock_user_func(client, message):
     & filters.user(SUDOERS)
     & ~filters.via_bot
 )
-@ASS_CLI_2.on_message(
-    filters.command("pfp", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_3.on_message(
-    filters.command("pfp", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_4.on_message(
-    filters.command("pfp", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_5.on_message(
-    filters.command("pfp", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
+
 async def set_pfp(client, message):
     if not message.reply_to_message or not message.reply_to_message.photo:
         return await eor(message, text="Reply to a photo.")
@@ -303,26 +186,6 @@ async def set_pfp(client, message):
 
 
 @ASS_CLI_1.on_message(
-    filters.command("bio", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_2.on_message(
-    filters.command("bio", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_3.on_message(
-    filters.command("bio", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_4.on_message(
-    filters.command("bio", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@ASS_CLI_5.on_message(
     filters.command("bio", prefixes=ASSISTANT_PREFIX)
     & filters.user(SUDOERS)
     & ~filters.via_bot
