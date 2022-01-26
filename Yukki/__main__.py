3c1dbf3de19d6a4fe91c2618d3baa8ad1b2dc3bd
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-01-26 07:29:51 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -10,15 +10,12 @@ from rich.console import Console
 from rich.table import Table
 from youtubesearchpython import VideosSearch
 
-from config import (LOG_GROUP_ID, LOG_SESSION, STRING1, STRING2, STRING3,
-                    STRING4, STRING5)
-from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
-                   ASSID1, ASSID2, ASSID3, ASSID4, ASSID5, ASSNAME1, ASSNAME2,
-                   ASSNAME3, ASSNAME4, ASSNAME5, BOT_ID, BOT_NAME, LOG_CLIENT,
+from config import (LOG_GROUP_ID, LOG_SESSION, STRING1)
+from Yukki import (ASS_CLI_1, 
+                   ASSID1,  ASSNAME1, BOT_ID, BOT_NAME, LOG_CLIENT,
                    OWNER_ID, app)
 from Yukki.Core.Clients.cli import LOG_CLIENT
-from Yukki.Core.PyTgCalls.Yukki import (pytgcalls1, pytgcalls2, pytgcalls3,
-                                        pytgcalls4, pytgcalls5)
+from Yukki.Core.PyTgCalls.Yukki import (pytgcalls1)
 from Yukki.Database import (get_active_chats, get_active_video_chats,
                             get_sudoers, is_on_off, remove_active_chat,
                             remove_active_video_chat)
@@ -106,11 +103,11 @@ async def initiate_bot():
         try:
             await ASS_CLI_1.send_message(
                 LOG_GROUP_ID,
-                "<b>Congrats!! Assistant Client 1  has started successfully!</b>",
+                "<b>Congrats!! Assistant Client LUNA has started successfully!</b>",
             )
         except Exception as e:
             print(
-                "\nAssistant Account 1 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
+                "\nAssistant Account LUNA has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
             console.print(f"\n[red]Stopping Bot")
             return
@@ -119,112 +116,15 @@ async def initiate_bot():
             await ASS_CLI_1.join_chat("YukkiSupport")
         except:
             pass
-        console.print(f"├[red] Assistant 1 Started as {ASSNAME1}!")
-        console.print(f"├[green] ID :- {ASSID1}!")
-    if STRING2 != "None":
-        try:
-            await ASS_CLI_2.send_message(
-                LOG_GROUP_ID,
-                "<b>Congrats!! Assistant Client 2 has started successfully!</b>",
-            )
-        except Exception as e:
-            print(
-                "\nAssistant Account 2 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
-            )
-            console.print(f"\n[red]Stopping Bot")
-            return
-        try:
-            await ASS_CLI_2.join_chat("OfficialYukki")
-            await ASS_CLI_2.join_chat("YukkiSupport")
+        console.print(f"├[red] Assistant Luna Started as {ASSNAME1}!")
+        console.print(f"├[green] ID :- {ASSID1}!")  
         except:
             pass
-        console.print(f"├[red] Assistant 2 Started as {ASSNAME2}!")
-        console.print(f"├[green] ID :- {ASSID2}!")
-    if STRING3 != "None":
-        try:
-            await ASS_CLI_3.send_message(
-                LOG_GROUP_ID,
-                "<b>Congrats!! Assistant Client 3 has started successfully!</b>",
-            )
-        except Exception as e:
-            print(
-                "\nAssistant Account 3 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
-            )
-            console.print(f"\n[red]Stopping Bot")
-            return
-        try:
-            await ASS_CLI_3.join_chat("OfficialYukki")
-            await ASS_CLI_3.join_chat("YukkiSupport")
-        except:
-            pass
-        console.print(f"├[red] Assistant 3 Started as {ASSNAME3}!")
-        console.print(f"├[green] ID :- {ASSID3}!")
-    if STRING4 != "None":
-        try:
-            await ASS_CLI_4.send_message(
-                LOG_GROUP_ID,
-                "<b>Congrats!! Assistant Client 4 has started successfully!</b>",
-            )
-        except Exception as e:
-            print(
-                "\nAssistant Account 4 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
-            )
-            console.print(f"\n[red]Stopping Bot")
-            return
-        try:
-            await ASS_CLI_4.join_chat("OfficialYukki")
-            await ASS_CLI_4.join_chat("YukkiSupport")
-        except:
-            pass
-        console.print(f"├[red] Assistant 4 Started as {ASSNAME4}!")
-        console.print(f"├[green] ID :- {ASSID4}!")
-    if STRING5 != "None":
-        try:
-            await ASS_CLI_5.send_message(
-                LOG_GROUP_ID,
-                "<b>Congrats!! Assistant Client 5 has started successfully!</b>",
-            )
-        except Exception as e:
-            print(
-                "\nAssistant Account 5 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
-            )
-            console.print(f"\n[red]Stopping Bot")
-            return
-        try:
-            await ASS_CLI_5.join_chat("OfficialYukki")
-            await ASS_CLI_5.join_chat("YukkiSupport")
-        except:
-            pass
-        console.print(f"├[red] Assistant 5 Started as {ASSNAME5}!")
-        console.print(f"├[green] ID :- {ASSID5}!")
-    if LOG_SESSION != "None":
-        try:
-            await LOG_CLIENT.send_message(
-                LOG_GROUP_ID,
-                "<b>Congrats!! Logger Client has started successfully!</b>",
-            )
-        except Exception as e:
-            print(
-                "\nLogger Client has failed to access the log Channel. Make sure that you have added your Logger Account to your log channel and promoted as admin!"
-            )
-            console.print(f"\n[red]Stopping Bot")
-            return
-        try:
-            await LOG_CLIENT.join_chat("OfficialYukki")
-            await LOG_CLIENT.join_chat("YukkiSupport")
-        except:
-            pass
+        
     console.print(f"└[red] Yukki Music Bot Boot Completed.")
     if STRING1 != "None":
         await pytgcalls1.start()
-    if STRING2 != "None":
-        await pytgcalls2.start()
-    if STRING3 != "None":
-        await pytgcalls3.start()
-    if STRING4 != "None":
-        await pytgcalls4.start()
-    if STRING5 != "None":
-        await pytgcalls5.start()
+    
     await idle()
     console.print(f"\n[red]Stopping Bot")
 
