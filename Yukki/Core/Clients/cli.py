b5e761591c09cdc8a1d5a7cc2ab386a6622774d9
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-01-26 07:00:22 +0530

    Update cli.py

--- Yukki/Core/Clients/cli.py
+++ Yukki/Core/Clients/cli.py
@@ -1,8 +1,6 @@
 from pyrogram import Client
 
-from config import (API_HASH, API_ID, BOT_TOKEN, LOG_SESSION, STRING1, STRING2,
-                    STRING3, STRING4, STRING5)
-
+from config import (API_HASH, API_ID, BOT_TOKEN, LOG_SESSION, STRING1)
 app = Client(
     "YukkiMusicBot",
     API_ID,
@@ -18,50 +16,6 @@ else:
         api_id=API_ID,
         api_hash=API_HASH,
         session_name=STRING1,
-        plugins=dict(root="Yukki.Plugins.Multi-Assistant"),
-    )
-
-
-if not STRING2:
-    ASS_CLI_2 = None
-else:
-    ASS_CLI_2 = Client(
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_name=STRING2,
-        plugins=dict(root="Yukki.Plugins.Multi-Assistant"),
-    )
-
-
-if not STRING3:
-    ASS_CLI_3 = None
-else:
-    ASS_CLI_3 = Client(
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_name=STRING3,
-        plugins=dict(root="Yukki.Plugins.Multi-Assistant"),
-    )
-
-
-if not STRING4:
-    ASS_CLI_4 = None
-else:
-    ASS_CLI_4 = Client(
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_name=STRING4,
-        plugins=dict(root="Yukki.Plugins.Multi-Assistant"),
-    )
-
-
-if not STRING5:
-    ASS_CLI_5 = None
-else:
-    ASS_CLI_5 = Client(
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_name=STRING5,
         plugins=dict(root="Yukki.Plugins.Multi-Assistant"),
     )
 
